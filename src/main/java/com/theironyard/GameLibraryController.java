599ab5f42406f9750ea0b830c6a7b9d8094c7bb7
DrScott <dougscott2@gmail.com>
2015-11-14 21:53:28 -0500

    working on joining tables properly

--- src/main/java/com/theironyard/GameLibraryController.java
+++ src/main/java/com/theironyard/GameLibraryController.java
@@ -28,6 +28,13 @@ public class GameLibraryController {
 
     @PostConstruct
     public void init() throws InvalidKeySpecException, NoSuchAlgorithmException, FileNotFoundException {
+        /*User user = users.findOneByName("Admin");
+        if(user == null){
+            user = new User();
+            user.name="Admin";
+            user.password = PasswordHash.createHash("password");
+            users.save(user);
+        }*/
 
         if (games.count() == 0){
             Scanner scanner = new Scanner(new File("games.csv"));
@@ -36,9 +43,11 @@ public class GameLibraryController {
                 String line = scanner.nextLine();
                 String[] columns = line.split(",");
                 Game g = new Game();
+                int id = Integer.valueOf(columns[2]);
+                User u = users.findOne(id);
+                g.user=u;
                 g.name = columns[0];
                 g.system=columns[1];
-               // g.user = user;
                 games.save(g);
             }
         }
@@ -50,13 +59,19 @@ public class GameLibraryController {
 
     public String home(HttpSession session,
                        Model model,
-                       String name,
-                       Integer system){
+                       String search,
+                       String system,
+                       String userGames){
 
         String username = (String) session.getAttribute("username");
 
         if (username == null){
             return "login";
+        }
+        if(userGames!=null){
+            model.addAttribute("games", users.findOneByName(username).games);
+        } else if(system != null){
+            model.addAttribute("games", games.findBySystem(system));
         }
         model.addAttribute("users", users.findAll());
         model.addAttribute("games", games.findAll());
--- src/main/java/com/theironyard/GameRepository.java
+++ src/main/java/com/theironyard/GameRepository.java
@@ -1,9 +1,16 @@
 package com.theironyard;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by DrScott on 11/13/15.
  */
 public interface GameRepository extends CrudRepository<Game, Integer>{
+    List<Game> findBySystem(String system);
+
+    @Query("SELECT g FROM Game g WHERE LOWER(name) LIKE '%' || LOWER(?) || '%'")
+    List<Game> searchByName(String search);
 }
